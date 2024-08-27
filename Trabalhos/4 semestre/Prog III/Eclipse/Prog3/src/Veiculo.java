69724feaf46bdb2b28ec0051fc3f937b7a658c51
Mauricio Cogo <Mauricioccogo@gmail.com>
2024-08-26 22:07:47 -0300

    Trabalho proIII calculadora em comptação grafica

---
+++ Trabalhos/4 semestre/Prog III/Eclipse/Prog3/src/Veiculo.java
@@ -0,0 +1,28 @@
+
+public class Veiculo {
+
+	private String modelo;
+	private String cor;
+	private int ano;
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public int getAno() {
+		return ano;
+	}
+
+}
